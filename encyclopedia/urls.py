52afe13b5a7db328cda6a5fa356e209ae6127a46
Galbur Sergey <gal_sergey@ukr.net>
2021-11-02 23:33:56 +0200

    Отображается форма редактирования

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('<str:url>',views.show_article, name ='show_article'),
     path("find/", views.find, name="find"),
-    path("create/new_page.html", views.createPage, name='new_page')
+    path("create/", views.createPage, name='new_page'),
+    path('<str:url>/edit/',views.edit_page, name ='edit_page')
     
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -21,7 +21,7 @@ def show_article(request,url):
     text = util.get_entry(url)
     if text :
         text = markdown2.markdown(text)
-        return render(request, 'encyclopedia/article.html', {'text':text})
+        return render(request, 'encyclopedia/article.html', {'title':url,'text':text})
     else:
         return render(request, 'encyclopedia/notfound.html', {'url':url})
 
@@ -42,21 +42,25 @@ def find(request):
 def createPage(request):
     if request.method == "POST":
         form = NewPageForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            text = form.cleaned_data['text']
-            file_name = 'entries/'+ title + '.md'
-            print(file_name)
+        if form.is_valid():     
+            title = form.cleaned_data['title']       
+            file_name = 'entries/'+ title + '.md'            
             if os.path.exists(file_name):
                 return render(request, "encyclopedia/page_exists.html", {"file_name": file_name})
             else:
+                
+                text = form.cleaned_data['text']
                 file = open(file_name, "w")
                 file.write("#" + title + "\r\n" + text)
                 file.close()
                 return HttpResponseRedirect(reverse("show_article", kwargs={'url':title}))
-
-
-
-
+        else:
+            return render(request, "encyclopedia/new_page.html", {"form": form.as_p})
 
     return render(request, "encyclopedia/new_page.html", {"form": NewPageForm().as_p})
+    
+def edit_page(request, url):
+    
+    text = util.get_entry(url)
+    return render(request, "encyclopedia/edit_page.html", {'form':NewPageForm({'title':url, 'text':text}), 'title':url})
+    return render(request, "encyclopedia/article.html", {"form": NewPageForm().as_p})
